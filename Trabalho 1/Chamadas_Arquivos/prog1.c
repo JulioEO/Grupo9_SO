929e78e6b3b90842f0c4c4a55d713b39c4c5e202
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-03-22 21:26:33 -0300

    Add files via upload

---
+++ Trabalho 1/Chamadas_Arquivos/prog1.c
@@ -0,0 +1,16 @@
+//Programa 1
+// syscall read(), open() e close()
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+int main(){
+	char texto[12];
+	int arq = open("arquivo",O_RDONLY); // abre o arquivo no modo READ ONLY
+	read(arq,texto,strlen("Hello World!")); //  le o arquivo
+	printf("%s\n", texto);
+	close(arq); // fecha o arquivo
+	return 0;
+}
