956b78d260a3f79c94a95b9a7c776798e5868dd6
JulioEO <40305649+JulioEO@users.noreply.github.com>
2020-03-27 20:33:40 -0300

    Update exemplo_processos.c

--- Trabalho 1/exemplo_processos.c
+++ Trabalho 1/exemplo_processos.c
@@ -10,13 +10,13 @@ int main(int argc, char const *argv[])//programa exemplo para as primitivas fork
 	int processo = fork();//cria um processo filho com fork()
 	if (processo==0){
 		printf("Sou o processo filho!\n");//imprime se for o processo filho
-		printf("Meu PID: %ld\n",getpid() );//imprime o pid do filho com getpid()
+		printf("Meu PID: %d\n",getpid() );//imprime o pid do filho com getpid()
 		exit(0);
 	}
 	else if(processo > 0){
 		wait(NULL);//espera o filho terminar com wait()
 		printf("Sou o processo pai! Espero o filho acabar para imprimir essa mensagem!\n");//imprime se for o processo pai
-		printf("Meu PID: %ld\n",getpid() );//imprime o id do pai com getpid()
+		printf("Meu PID: %d\n",getpid() );//imprime o id do pai com getpid()
 	}
 	else{
 		printf("Erro na criacao do processo!\n" );//imprime se houve erro na criacao do processo
