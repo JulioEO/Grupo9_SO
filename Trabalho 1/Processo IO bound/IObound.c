82844fd137953289183f2c0be8c60bdb498d673f
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-03-28 16:10:32 -0300

    Update IObound.c

--- Trabalho 1/Processo IO bound/IObound.c
+++ Trabalho 1/Processo IO bound/IObound.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
-	char string[1000];
+	char string[1001];
 	puts("Digite algo para ser invertido:");
 	fgets(string,1000,stdin); // le uma string
 	for(int i = strlen(string) -1; i>=0 ;i--) putchar(string[i]); //imprimi a string invertida 
