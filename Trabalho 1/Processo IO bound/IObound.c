9f9a4d4d6372b2f07fe1f36ceffaa8981b5930f5
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-03-28 16:08:05 -0300

    Add files via upload

---
+++ Trabalho 1/Processo IO bound/IObound.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+	char string[1000];
+	puts("Digite algo para ser invertido:");
+	fgets(string,1000,stdin); // le uma string
+	for(int i = strlen(string) -1; i>=0 ;i--) putchar(string[i]); //imprimi a string invertida 
+	putchar('\n');
+	return 0;
+}
