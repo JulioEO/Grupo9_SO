075828ee6e3aa81db490273ab152c09a7bc372cf
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-03-28 15:50:06 -0300

    Update prog1.c

--- Trabalho 1/Chamadas_Arquivos/prog1.c
+++ Trabalho 1/Chamadas_Arquivos/prog1.c
@@ -9,7 +9,7 @@
 int main(){
 	char texto[12];
 	int arq = open("arquivo",O_RDONLY); // abre o arquivo no modo READ ONLY
-	read(arq,texto,strlen("Hello World!")); //  le o arquivo
+	read(arq,texto,strlen("Arquivo lido com sucesso.")); //  le o arquivo
 	printf("%s\n", texto);
 	close(arq); // fecha o arquivo
 	return 0;
