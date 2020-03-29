0a3c165265d86b667f4a1482c62000ae4cced5bc
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-03-29 19:49:01 -0300

    Add files via upload

---
+++ Trabalho 1/Chamadas_Arquivos/prog1.c
@@ -0,0 +1,25 @@
+//Programa 1
+// syscall read(), open() e close()
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(){
+	char texto[26];
+	char path[100];
+	getcwd(path,100);
+	strncat(path,"/arquivo",8);
+	int arq = open(path,O_RDONLY);
+	int b = read(arq,texto,25);
+	texto[25] = '\0';
+	puts(path);
+	int a = close(arq);
+	return 0;
+}
+
+
+
