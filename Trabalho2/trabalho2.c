e01b5bbf9d6e1b1dc4390a12dd46097cacacdbb2
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-04-27 20:42:04 -0300

    Add files via upload

---
+++ Trabalho2/trabalho2.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#define TAMANHO 20// tamanho do buffer
+#define R 30 //R * TAMANHO é o número de repeticoes do produtor e do consumidor
+//compilar com : gcc -pthread -o trabalho2 trabalho2.c
+pthread_mutex_t lock;
+pthread_cond_t dormir;
+int k = 0;
+
+typedef struct{
+          int lista[TAMANHO];
+          int livre;
+}pilha;
+
+void *consumir( void *);
+void *produzir( void *);
+void regiao_critica(pilha*,int* , int);
+
+int main(){
+
+     pilha *buffer;
+     //inicializa o buffer
+     for(int i = 0; i< TAMANHO; i++) buffer->lista[i] = -1;
+     buffer->livre = 0;
+
+
+     pthread_t prod, cons;
+	int p,c;
+
+	p = pthread_create( &prod, NULL, produzir,(void*) buffer);
+	c = pthread_create( &cons, NULL, consumir,(void*) buffer);
+     //checa se os threads foram criados sem erros
+	if(c != 0 || p != 0 ){
+		puts("ERRO\n");
+		exit(0);
+	}
+
+     pthread_join( cons, NULL);
+     pthread_join( prod, NULL);
+     puts("\n\nFIM!!!!!!!\n\n");
+
+	return 0;
+}
+
+// le o ultimo numero gravado no buffer
+void *consumir( void *ptr){
+     pilha* b = (pilha*) ptr;
+     int n;
+
+     for(int j = 0; j<R*TAMANHO;j++){//repeticoes, para testar o codigo
+          regiao_critica(b, &n , -1);
+     }
+     pthread_cond_signal(&dormir);
+}
+
+
+// produz um numero aleatório e grava no buffer
+void *produzir( void *ptr){
+     pilha* b = (pilha*) ptr;
+     srand(time(0));
+     int a;
+     for(int j = 0; j<R*TAMANHO;j++){//repeticoes, para testar o codigo
+          a = rand() % 100;
+          regiao_critica(b ,NULL , a);
+     }
+     pthread_cond_signal(&dormir);
+}
+// regiao critica
+void regiao_critica(pilha* buf,int *c, int p){
+
+     pthread_mutex_lock(&lock);
+
+     //escreve noo buffer
+     if (c == NULL){
+          if (buf->livre == 0) pthread_cond_signal(&dormir);
+          if(buf->livre < TAMANHO){
+               buf->lista[buf->livre] = p;
+               printf("%d - Produzido %d posicao %d\n", ++k, buf->lista[buf->livre], buf->livre);
+               buf->livre++;
+          }else pthread_cond_wait(&dormir,&lock);
+          
+     }
+     //le do buffer
+     else if (p == -1){
+          if (buf->livre == TAMANHO) pthread_cond_signal(&dormir);
+          if(buf->livre > 0){
+               buf->livre--;
+               printf("%d - Consumido %d posicao %d\n",++k,  buf->lista[buf->livre], buf->livre);
+               *c = buf->lista[buf->livre];
+               buf->lista[buf->livre] = -1;
+          }else pthread_cond_wait(&dormir,&lock);
+     }
+     pthread_mutex_unlock(&lock);
+}
