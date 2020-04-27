d9f38994d8c787ebc589f89e2cbf85fb2603155d
VitorLeonardi <61316876+VitorLeonardi@users.noreply.github.com>
2020-04-27 18:12:05 -0300

    Add files via upload
    fiz uma modificaçoes. Ta um pouco melhor que o ultimo, mas ainda não funciona direito

---
+++ Trabalho2/trabalho2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#define TAMANHO 50 // tamanho do buffer
+//compilar com : gcc -pthread -o trabalho2 trabalho2.c
+pthread_mutex_t lock;
+
+
+typedef struct{
+          int lista[TAMANHO];
+          int livre;
+}pilha;
+
+void *consumir( void *);
+void *produzir( void *);
+void regiao_critica(pilha* buf,int* , int);
+
+int main(){
+
+     pilha *buffer;
+     //inicializa o buffer
+     for(int i = 0; i< TAMANHO; i++) (*buffer).lista[i] = -1;
+     (*buffer).livre = 0;
+
+
+     pthread_t prod, cons;
+	int p,c;
+
+	p = pthread_create( &prod, NULL, produzir,(void*) buffer);
+	c = pthread_create( &cons, NULL, consumir,(void*) buffer);
+     //checa se os threads foram criados sem erros
+	if(c != 0 || p !=0 ){
+		puts("ERRO\n");
+		exit(0);
+	}
+
+     pthread_join( cons, NULL);
+     pthread_join( prod, NULL);
+
+	return 0;
+}
+
+// le o ultimo numero gravado no buffer
+void *consumir( void *ptr){
+     pilha* b = (pilha*) ptr;
+     int n;
+     for(int j = 0; j<100*TAMANHO;j++){//repeticoes, para testar o codigo
+          regiao_critica(b, &n , -1);
+          printf("Consumido %d posicao %d\n", n, b->livre -1);
+     }
+}
+
+
+// produz um numero aleatório e grava no buffer
+void *produzir( void *ptr){
+     pilha* b = (pilha*) ptr;
+     srand(time(0));
+     for(int j = 0; j<100*TAMANHO;j++){//repeticoes, para testar o codigo
+          int a = rand() % 100;
+          regiao_critica(b ,NULL , a);
+          printf("Produzido %d posicao %d\n", a, b->livre -1);
+     }
+}
+// regiao critica
+void regiao_critica(pilha* buf,int *c, int p){
+
+     pthread_mutex_lock(&lock);
+
+     //escreve noo buffer
+     if (c == NULL && buf->livre < TAMANHO){
+          buf->lista[buf->livre] = p;
+          buf->livre++;
+     }
+     //le do buffer
+     else if(p = -1 && buf->livre > 0){
+          buf->livre--;
+          *c = buf->lista[buf->livre];
+          buf->lista[buf->livre +1 ] = -1;
+     }
+     pthread_mutex_unlock(&lock);
+}
